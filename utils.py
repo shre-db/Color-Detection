bd92683cabf8332d4475b6189bd44cf940d31961
shre-db <shreyasdb99@gmail.com>
2023-05-13 13:43:33 +0530

    Add main and utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+
+
+def get_limits(color):
+
+    c = np.uint8([[color]])  # Insert the bgr values which you want to convert to hsv
+    hsvC = cv.cvtColor(c, cv.COLOR_BGR2HSV)
+
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    upperLimit = hsvC[0][0][0] + 10, 255, 255
+
+    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+    upperLimit = np.array(upperLimit, dtype=np.uint8)
+
+    return lowerLimit, upperLimit
